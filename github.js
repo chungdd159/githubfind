ff2ab1467ad86ee206ec02a73dca0b0305fa4714
chungdd <dchung1509@gmail.com>
2020-05-25 14:43:48 +0700

    upid

--- github.js
+++ github.js
@@ -1,22 +1,27 @@
 class Github {
   constructor() {
-    this.client_id = 'd9308aacf8b204d361fd';
-    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+    this.client_id = 'a5822bec44a6d3fe4677';
+    this.client_secret = '0f937d3a526257d70d0b5947f4304a85d3808076';
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
 
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
 
     const profile = await profileResponse.json();
     const repos = await repoResponse.json();
+    console.log(profile);
 
     return {
       profile,
-      repos
-    }
+      repos,
+    };
   }
 }
